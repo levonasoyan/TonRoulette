3e2607fede5eeb427b0cf47da8b8cb629734c6fc
levonasoyan <levonasoyan1@gmail.com>
2024-07-15 23:15:58 +0400

    initial

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,60 @@
-import { Box, Typography } from "@mui/material";
+import { Avatar, Box, Button, Typography } from "@mui/material";
 
 const Home = () => {
   return (
     <>
-      <Box>
-        <Typography>Hello</Typography>
-        <Typography>Hello</Typography>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        width="100%"
+        height="100vh"
+      >
+        <Box m="100px 0px">
+          <Typography fontWeight={400}>Current Pool</Typography>
+          <Typography
+            fontWeight={500}
+            fontSize={30}
+            color="blue"
+            textAlign="center"
+          >
+            {" "}
+            500$
+          </Typography>
+        </Box>
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          gap="15px"
+        >
+          <Avatar sizes="200px"></Avatar>
+          <Typography fontWeight={85}>Tenshi_6666</Typography>
+        </Box>
+        <Box
+          mt="20px"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          width="100%"
+        >
+          <Typography
+            fontWeight={500}
+            fontSize={12}
+            textAlign="center"
+            m="20px 0px"
+            width="80%"
+          >
+            By clicking this button you will bet 5$ work of TON <br></br> (4$
+            will go to pool)
+          </Typography>
 
-        <Typography>Hello</Typography>
-
-        <Typography>Hello</Typography>
-
-        <Typography>Hello</Typography>
+          <Button variant="outlined" color="primary" style={{ width: "80%" }}>
+            Bet TON
+          </Button>
+        </Box>
       </Box>
     </>
   );
