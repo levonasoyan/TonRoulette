ddf8498a3622a7f3273c8f93470e09ef11229c6d
levonasoyan <levonasoyan1@gmail.com>
2024-07-15 22:32:26 +0400

    fix: ui

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+const Home = () => {
+  return (
+    <>
+      <Box>
+        <Typography>Hello</Typography>
+      </Box>
+    </>
+  );
+};
+
+export default Home;
